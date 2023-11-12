eab8a91c65a39c50d7127fd18fcf60ac14c1a312
Alireza Karavi <karavi.alireza@gmail.com>
2023-11-12 14:44:00 +0330

    indo

--- src/app/modules/errors/errors.component.ts
+++ src/app/modules/errors/errors.component.ts
@@ -1,13 +1,5 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  DrawerComponent,
-  MenuComponent,
-  ScrollComponent,
-  ScrollTopComponent,
-  StickyComponent,
-  ToggleComponent,
-} from '../../_metronic/kt/components';
 
 @Component({
   selector: 'app-errors',
@@ -16,19 +8,19 @@ import {
 })
 export class ErrorsComponent implements OnInit {
   @HostBinding('class') class = 'd-flex flex-column flex-root';
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   routeToDashboard() {
     this.router.navigate(['dashboard']);
-    setTimeout(() => {
-      ToggleComponent.bootstrap();
-      ScrollTopComponent.bootstrap();
-      DrawerComponent.bootstrap();
-      StickyComponent.bootstrap();
-      MenuComponent.bootstrap();
-      ScrollComponent.bootstrap();
-    }, 200);
+    // setTimeout(() => {
+    //   ToggleComponent.bootstrap();
+    //   ScrollTopComponent.bootstrap();
+    //   DrawerComponent.bootstrap();
+    //   StickyComponent.bootstrap();
+    //   MenuComponent.bootstrap();
+    //   ScrollComponent.bootstrap();
+    // }, 200);
   }
 }
