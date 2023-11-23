ae8408b544eda81986c72535922b28245591fb10
karavi <karavi@ntk.ir>
2023-11-23 19:14:31 +0330

    indo

--- src/app/components/header-bar/header-bar.component.ts
+++ src/app/components/header-bar/header-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenInfoModel } from 'ntk-cms-api';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { PublicHelper } from 'src/app/core/helpers/publicHelper';
 import { TokenHelper } from 'src/app/core/helpers/tokenHelper';
 import { ThemeStoreModel } from 'src/app/core/models/themeStoreModel';
 import { CmsStoreService } from 'src/app/core/reducers/cmsStore.service';
+import { PageInfoService } from 'src/app/core/services/page-info.service';
 import { ThemeService, ThemeModeType } from 'src/app/core/services/theme.service';
 
 @Component({
@@ -18,7 +19,7 @@ export class HeaderBarComponent implements OnInit {
     public tokenHelper: TokenHelper,
     private publicHelper: PublicHelper,
     private themeService: ThemeService,
-
+    private pageInfoService: PageInfoService,
   ) {
     this.tokenHelper.getCurrentToken().then((value) => {
       this.tokenInfo = value;
@@ -34,8 +35,10 @@ export class HeaderBarComponent implements OnInit {
   cmsApiStoreSubscribe: Subscription;
   tokenInfo = new TokenInfoModel();
   themeStore = new ThemeStoreModel();
+  title$: Observable<string>;
 
   ngOnInit(): void {
+    this.title$ = this.pageInfoService.title.asObservable();
   }
   ngOnDestroy() {
     this.cmsApiStoreSubscribe.unsubscribe();
