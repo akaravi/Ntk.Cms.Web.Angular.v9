2ad157e95c707a0d57a3a1dc131ae87299315911
karavi <karavi@ntk.ir>
2023-12-04 06:39:14 +0330

    menu update

--- src/app/components/footer-bar/footer-bar.component.ts
+++ src/app/components/footer-bar/footer-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenInfoModel } from 'ntk-cms-api';
+import { Subscription } from 'rxjs';
 import { PublicHelper } from 'src/app/core/helpers/publicHelper';
+import { TokenHelper } from 'src/app/core/helpers/tokenHelper';
 import { ThemeStoreModel } from 'src/app/core/models/themeStoreModel';
 import { ThemeService } from 'src/app/core/services/theme.service';
 
@@ -11,17 +14,30 @@ import { ThemeService } from 'src/app/core/services/theme.service';
 export class FooterBarComponent implements OnInit {
 
   constructor(
+    public tokenHelper: TokenHelper,
     private publicHelper: PublicHelper,
-    private themeService:ThemeService
-  ) { }
-  themeStore = new ThemeStoreModel();
+    private themeService: ThemeService
+  ) {
+    this.tokenHelper.getCurrentToken().then((value) => {
+      this.tokenInfo = value;
+    });
+    this.cmsApiStoreSubscribe = this.tokenHelper.getCurrentTokenOnChange().subscribe((value) => {
+      this.tokenInfo = value;
 
+    });
+  }
+  themeStore = new ThemeStoreModel();
+  cmsApiStoreSubscribe: Subscription;
+  tokenInfo = new TokenInfoModel();
   ngOnInit(): void {
     this.publicHelper.getReducerCmsStoreOnChange().subscribe((value) => {
       this.themeStore = value.themeStore;
     });
   }
+  ngOnDestroy() {
+    this.cmsApiStoreSubscribe.unsubscribe();
+  }
   onActionCleanDataMenu(): void {
     this.themeService.cleanDataMenu();
-}
+  }
 }
