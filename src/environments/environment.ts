ef99a24ffa1a9d418aab316563737fbac4d56a52
Alireza Karavi <karavi.alireza@gmail.com>
2024-01-29 17:15:48 +0330

    update ver

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,8 +17,8 @@ export const environment = {
   mainTitle: 'سامانه مدیریتی محتوا',
   cmsServerConfig: {
     configApiRetry: 1,
-    configApiServerPath: 'https://apicms.ir/api/v2/',
-    //configApiServerPath: 'https://localhost:2390/api/v2/', // Test Api
+    //configApiServerPath: 'https://apicms.ir/api/v2/',
+    configApiServerPath: 'https://localhost:2390/api/v2/', // Test Api
     //configApiServerPath: 'https://6f4pj689-7130.euw.devtunnels.ms/api/v2/', // Test Api
     //configApiServerPath: 'https://localhost:44342/api/v2/', // Test Api Docer
     configHubServerPath: 'https://apicms.ir/hub/',
