d0a5a15b40337a8716a5a38cf458cd58e56f0b1e
Alireza Karavi <karavi.alireza@gmail.com>
2024-05-21 18:08:10 +0330

    indo

--- src/app/modules/pages/page-aboutus/page-aboutus.component.ts
+++ src/app/modules/pages/page-aboutus/page-aboutus.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { TokenInfoModel } from 'ntk-cms-api';
+import { CoreSiteModel, CoreSiteService, ErrorExceptionResult, TokenInfoModel } from 'ntk-cms-api';
 import { Subscription } from 'rxjs';
 import { TokenHelper } from 'src/app/core/helpers/tokenHelper';
+import { ProgressSpinnerModel } from 'src/app/core/models/progressSpinnerModel';
+import { CmsToastrService } from 'src/app/core/services/cmsToastr.service';
 import { PageInfoService } from 'src/app/core/services/page-info.service';
 
 @Component({
@@ -11,25 +13,58 @@ import { PageInfoService } from 'src/app/core/services/page-info.service';
 })
 export class PageAboutusComponent implements OnInit {
 
-  constructor(public pageInfo: PageInfoService, public translate: TranslateService,private tokenHelper: TokenHelper,) { 
+  constructor(public pageInfo: PageInfoService,
+    public translate: TranslateService,
+    private tokenHelper: TokenHelper,
+    private coreSiteService: CoreSiteService,
+    private cmsToastrService: CmsToastrService,
+  ) {
 
     this.tokenHelper.getCurrentToken().then((value) => {
       this.tokenInfo = value;
-      
+      if (this.tokenInfo.siteId > 0)
+        this.SiteInfo(this.tokenInfo.siteId);
+      else
+      this.SiteInfo(0);
     });
     this.cmsApiStoreSubscribe = this.tokenHelper.getCurrentTokenOnChange().subscribe((next) => {
       this.tokenInfo = next;
-      
+      if (this.tokenInfo.siteId > 0)
+        this.SiteInfo(this.tokenInfo.siteId);
+      else
+      this.SiteInfo(0);
     });
 
   }
   cmsApiStoreSubscribe: Subscription;
   tokenInfo = new TokenInfoModel();
+  loading = new ProgressSpinnerModel();
+  dataModelResult: ErrorExceptionResult<CoreSiteModel> = new ErrorExceptionResult<CoreSiteModel>();
 
   ngOnInit(): void {
     this.pageInfo.updateTitle(this.translate.instant('ACTION.ABOUT'));
   }
   ngOnDestroy(): void {
     this.cmsApiStoreSubscribe.unsubscribe();
+  }
+  SiteInfo(linkSiteId: number): void {
+    const pName = this.constructor.name + 'main';
+    this.loading.Start(pName);
+
+    this.coreSiteService.ServiceMasterSiteInfo(linkSiteId).subscribe({
+      next: (ret) => {
+        if (ret.isSuccess) {
+          this.dataModelResult = ret;
+        } else {
+          this.cmsToastrService.typeErrorMessage(ret.errorMessage);
+        }
+        this.loading.Stop(pName);
+      },
+      error: (er) => {
+        this.cmsToastrService.typeError(er);
+        this.loading.Stop(pName);
+      }
+    }
+    );
   }
 }
