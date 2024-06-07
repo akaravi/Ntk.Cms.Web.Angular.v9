55b6ef876c211ec9cefdb891ed6880dd086d0990
karavi <karavi@ntk.ir>
2024-06-07 09:29:57 +0330

    update step 1

--- src/app/cms-modules/core-main/site/coreSite.resolver.ts
+++ src/app/cms-modules/core-main/site/coreSite.resolver.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
+
 import { CoreSiteService, FilterModel } from 'ntk-cms-api';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class CoreSiteResolver implements Resolve<any> {
+export class CoreSiteResolver  {
 
     filterModel = new FilterModel();
 
