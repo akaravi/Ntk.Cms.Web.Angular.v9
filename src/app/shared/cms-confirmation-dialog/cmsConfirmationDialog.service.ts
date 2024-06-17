b97c8d869c4c40a06b86e1da21d41f81ce8d00df
karavi <karavi@ntk.ir>
2024-06-17 16:43:15 +0330

    indo

--- src/app/shared/cms-confirmation-dialog/cmsConfirmationDialog.service.ts
+++ src/app/shared/cms-confirmation-dialog/cmsConfirmationDialog.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
 import { TokenHelper } from 'src/app/core/helpers/tokenHelper';
 import { environment } from 'src/environments/environment';
 import { ConfirmationDialogComponent } from './cmsConfirmationDialog.component';
@@ -16,19 +17,25 @@ export class ConfirmDialogModel {
 }
 @Injectable()
 export class CmsConfirmationDialogService {
-  constructor(public dialog: MatDialog, public tokenHelper: TokenHelper,) { }
+  constructor(public dialog: MatDialog, public tokenHelper: TokenHelper,
+    public translate: TranslateService,
+
+  ) { }
   public confirm(
     title: string,
     message: string,
-    btnConfirmText: string = 'Confirm',
-    btnCancelText: string = 'Cancel',
+    btnConfirmText: string = '',
+    btnCancelText: string = '',
     dialogSize: 'sm' | 'lg' = 'sm'): Promise<boolean> {
     const dialogData = new ConfirmDialogModel(title, message);
     dialogData.btnConfirmText = btnConfirmText;
     dialogData.btnCancelText = btnCancelText;
     dialogData.dialogSize = dialogSize;
 
-
+    if (!dialogData.btnConfirmText || dialogData.btnConfirmText == '' || dialogData.btnConfirmText.length == 0)
+      this.translate.get('ACTION.CONFIRM').subscribe((str: string) => { dialogData.btnConfirmText = str; });
+    if (!dialogData.btnCancelText || dialogData.btnCancelText == '' || dialogData.btnCancelText.length == 0)
+      this.translate.get('ACTION.CANCEL').subscribe((str: string) => { dialogData.btnCancelText = str; });
     //open poup
     var panelClass = '';
     if (this.tokenHelper.isMobile)
