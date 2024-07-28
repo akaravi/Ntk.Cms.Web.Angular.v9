81a04906302c8acf09e6454e765f6ebb47bba22a
Alireza Karavi <karavi.alireza@gmail.com>
2024-07-28 15:13:29 +0330

    indo

--- src/app/shared/progress-spinner/progress-spinner.component.ts
+++ src/app/shared/progress-spinner/progress-spinner.component.ts
@@ -1,13 +1,8 @@
 import {
-  OverlayConfig,
-  OverlayRef
-} from '@angular/cdk/overlay';
-import {
-  Component, DoCheck, Input,
-  OnInit, TemplateRef, ViewChild, ViewContainerRef
+  Component,
+  Input,
+  OnInit
 } from '@angular/core';
-import { ProgressSpinnerModel } from 'src/app/core/models/progressSpinnerModel';
-import { OverlayService } from '../overlay/overlay.service';
 import { ProcessService } from 'src/app/core/services/process.service';
 
 @Component({
@@ -23,7 +18,7 @@ export class ProgressSpinnerComponent implements OnInit {
   ) {
     this.processService.processInRun
   }
-  @Input()  optionsInfoAreaId: string='global';
+  @Input() optionsInfoAreaId: string = 'global';
   ngOnInit(): void {
 
   }
